07fbe6bbb603c21f3f58d990b8dc5f6c4364938c
Trevor Pennington <43477475+TrevPennington@users.noreply.github.com>
2024-06-10 20:58:19 -0400

    add second player, add boundaries

--- game.py
+++ game.py
@@ -8,7 +8,10 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+player_radius = 40
+
+player_one_pos = pygame.Vector2(screen.get_width() * 0.25, screen.get_height() / 2)
+player_two_pos = pygame.Vector2(screen.get_width() * 0.75, screen.get_height() / 2)
 
 while running:
     # poll for events
@@ -20,17 +23,50 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("blue")
 
-    pygame.draw.circle(screen, "red", player_pos, 40)
+    # Player 1
+    pygame.draw.circle(screen, "red", player_one_pos, player_radius)
 
+    # Play 2
+    pygame.draw.circle(screen, "yellow", player_two_pos, player_radius)
+
+
+    # Player 1 controls
     keys = pygame.key.get_pressed()
+    # MOVE UP
     if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
+        if player_one_pos.y - player_radius > 0:
+            player_one_pos.y -= 300 * dt
+    # MOVE DOWN
     if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
+        if player_one_pos.y + player_radius < screen.get_height():
+            player_one_pos.y += 300 * dt
+    # MOVE LEFT
     if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
+        if player_one_pos.x - player_radius > 0:
+            player_one_pos.x  -= 300 * dt
+    # MOVE RIGHT
     if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+        if player_one_pos.x + player_radius < screen.get_width():
+            player_one_pos.x += 300 * dt
+
+    # Player 2 controls
+    keys = pygame.key.get_pressed()
+    # MOVE UP
+    if keys[pygame.K_UP]:
+        if player_two_pos.y - player_radius > 0:
+            player_two_pos.y -= 300 * dt
+    # MOVE DOWN
+    if keys[pygame.K_DOWN]:
+        if player_two_pos.y + player_radius < screen.get_height():
+            player_two_pos.y += 300 * dt
+    # MOVE LEFT
+    if keys[pygame.K_LEFT]:
+        if player_two_pos.x - player_radius > 0:
+            player_two_pos.x -= 300 * dt
+    # MOVE RIGHT
+    if keys[pygame.K_RIGHT]:
+        if player_two_pos.x + player_radius < screen.get_width():
+            player_two_pos.x += 300 * dt
 
     # flip() the display to put your work on screen
     pygame.display.flip()
